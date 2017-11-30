87023fa3faac46b9e9c103c219f591fe6b4773a4
Box-Bop <Box-Bop@github.com>
2017-11-30 08:41:53 +0200

    Backup

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var shape1 = new Shape();
+
+            shape1.X = 10;
+            shape1.Y = 20;
+            shape1.Widht = 100;
+            shape1.Height = 50;
+
+            var circle1 = new Circle();
+
+            circle1.X = 20;
+            circle1.Y = 10;
+            circle1.Widht = 100;
+            circle1.Height = 300;
+
+            circle1.Draw();
+            shape1.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Shape
+    {
+
+        public int X {get; set; }
+        public int Y { get; set; }
+        public int Height { get; set; }
+        public int Widht { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("*Drawn* ");
+        }
+    }
+}
